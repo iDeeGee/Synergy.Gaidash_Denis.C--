a53df007cd8013c5e3483ebdc51fe55dd351d1e7
iDeeGee <denisgaidash@gmail.com>
2024-09-08 22:10:53 +0300

    T4+T5.Synergy.GaidashDenis

---
+++ T4_Synergy.GaidashDenis.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+std::mutex mtx;  
+std::condition_variable cv;  
+int counter = 1;  
+bool turn_even = false;  
+
+// нечётные числа
+void PrintOdd() {
+    while (counter <= 10) {
+        std::unique_lock<std::mutex> lock(mtx);  
+        cv.wait(lock, [] { return !turn_even; });  
+        if (counter > 10) break;  
+        std::cout << "Odd: " << counter << std::endl;
+        counter++;  
+        turn_even = true;  
+        cv.notify_one();  
+    }
+}
+
+// чётные числа
+void PrintEven() 
+{
+    while (counter <= 10) {
+        std::unique_lock<std::mutex> lock(mtx);  
+        cv.wait(lock, [] { return turn_even; });  
+        if (counter > 10) break;  
+        std::cout << "Even: " << counter << std::endl;
+        counter++;  
+        turn_even = false;  
+        cv.notify_one(); 
+    }
+}
+
+int main() 
+{     
+    std::thread t1(PrintOdd);
+    std::thread t2(PrintEven);
+    
+    t1.join();
+    t2.join();
+
+    return 0;
+}
---
+++ T5_Synergy.GaidashDenis.cpp
@@ -0,0 +1,107 @@
+﻿#include <iostream>
+
+struct Node 
+{
+    int data;        
+    Node* next;      
+
+    Node(int value) : data(value), next(nullptr) {}
+};
+
+
+class SinglyLinkedList 
+{
+public:
+    Node* head;  
+
+    SinglyLinkedList() : head(nullptr) {}
+
+    // добавления элемента в начало списка
+    void Push(int value) 
+    {
+        Node* newNode = new Node(value);
+        newNode->next = head;
+        head = newNode;
+    }
+
+    // вывод элементов списка
+    void PrintList() 
+    {
+        Node* temp = head;
+        while (temp != nullptr) 
+        {
+            std::cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        std::cout << "конец списка" << std::endl;
+    }
+
+    // сортировка вставками
+    void InsertionSort() 
+    {
+        if (!head || !head->next) return;  
+
+        Node* sorted = nullptr;  
+        Node* current = head;
+
+        
+        while (current != nullptr) 
+        {
+            Node* next = current->next;  
+            sorted = sortedInsert(sorted, current); 
+            current = next;  
+        }
+
+        head = sorted;  
+    }
+
+private:
+    
+    Node* sortedInsert(Node* sorted, Node* newNode) 
+    {        
+        if (!sorted || sorted->data >= newNode->data) 
+        {
+            newNode->next = sorted;
+            return newNode;
+        }
+
+        // поиск места
+        Node* current = sorted;
+        while (current->next != nullptr && current->next->data < newNode->data) 
+        {
+            current = current->next;
+        }
+
+        newNode->next = current->next;
+        current->next = newNode;
+
+        return sorted;
+    }
+};
+
+int main() 
+{
+    setlocale(LC_ALL, "Rus");
+
+    SinglyLinkedList list;
+    
+    list.Push(5);
+    list.Push(3);
+    list.Push(6);
+    list.Push(8);
+    list.Push(1);
+    list.Push(2);
+    list.Push(4);
+    list.Push(7);
+    list.Push(9);
+
+    std::cout << "Исходный список:" << std::endl;
+    list.PrintList();
+    
+    list.InsertionSort();
+
+    std::cout << "Отсортированный список:" << std::endl;
+    list.PrintList();
+
+    return 0;
+}
